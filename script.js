242ee5d69c085d8c9db2ae560c87504b4f5786c2
Yakraj Pariyar <yakraj@users.noreply.github.com>
2020-05-05 11:56:50 +0530

    Successfully added all files.

---
+++ script.js
@@ -0,0 +1,6 @@
+function clickme(){
+  var box = document.getElementById("navbox");
+  var imagebox = document.querySelector('.images');
+  box.classList.toggle("container");
+  imagebox.classList.toggle("mimages");
+}
